cf193ecb54efaa86d72a588eccdaf047626aa479
xocelyk <kylecox2000@gmail.com>
2022-11-15 22:45:45 -0600

    change title

--- streamlit-app.py
+++ streamlit-app.py
@@ -98,8 +98,8 @@ def make_plot(df):
     away_score = df['away_score'].iloc[-1]
     current_prob = df['home_win_prob'].iloc[-1]
     score_string = str(home_score) + ' - ' + str(away_score)
-    vs_string = home_name +  ' ' + ' ' + away_name 
-    current_prob_string = 'Current Win Probability: ' + str(round(100 * current_prob, 1)) + '%'
+    vs_string = home_name +  ' ' + 'vs.' + ' ' + away_name 
+    current_prob_string = 'Current ' + home_name + 'Win Probability: ' + str(round(100 * current_prob, 1)) + '%'
     title = vs_string + '<br>' + score_string + '<br>' + '<sup>' + current_prob_string + '</sup>'
     ylabel = home_name + ' Win Probability'
 
